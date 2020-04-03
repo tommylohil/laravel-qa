1cd36d6ce583f5950560aff93e3f1540085835e9
tommylohil <tommylohil23052000@gmail.com>
2020-04-03 17:06:01 +0700

    Added author info and question creation date in question item

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -14,9 +14,12 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        $questions = Question::latest()->paginate(5);
+        // \DB::enableQueryLog();
+        $questions = Question::with('user')->latest()->paginate(5);
 
-        return view('questions.index', compact('questions'));
+        return view('questions.index', compact('questions')); //->render();
+        // dd(\DB::getQueryLog());
+        
     }
 
     /**
--- app/Model/Question.php
+++ app/Model/Question.php
@@ -12,12 +12,23 @@ class Question extends Model
         'title', 'body'
     ];
 
-    public function user() {
+    public function user() 
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function setTitleAttribute($value) {
+    public function setTitleAttribute($value) 
+    {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function getUrlAttribute() 
+    {
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute() {
+        return $this->created_at->diffForHumans();
     }
 }
