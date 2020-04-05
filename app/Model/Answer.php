aa5a7bb97f1c283306d44f4d460dfed0f3a81b36
tommylohil <tommylohil23052000@gmail.com>
2020-04-05 07:04:38 +0700

    Implemented delete functionality for answer

--- app/Model/Answer.php
+++ app/Model/Answer.php
@@ -42,5 +42,9 @@ class Answer extends Model
         static::created(function ($answer) {
             $answer->question->increment('answers_count');
         });
+
+        static::deleted(function ($answer) {
+            $answer->question->decrement('answers_count');
+        });
     }
 }
