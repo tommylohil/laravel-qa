c9e716f35e2de5fdf450e3554af7ff4b1371ef32
tommylohil <tommylohil23052000@gmail.com>
2020-04-10 14:06:26 +0700

    Added forgotten Favorite.vue

---
+++ resources/js/components/Favorite.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <a title="Click to mark as favorite question (Click again to undo)" 
+            :class="classes"
+            @click.prevent="toggle"
+        >
+                <i class="fas fa-star fa-2x"></i>
+            <span class="favorites-count">{{ count }}</span>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['question'],
+
+    data () {
+        return {
+            isFavorited: this.question.is_favorited,
+            count: this.question.favorites_count,
+            id: this.question.id
+        }
+    },
+
+    computed: {
+        classes () {
+            return [
+                'favorite',
+                'mt-2',
+                !this.signedIn ? 'off' : (this.isFavorited ? 'favorited' : ''),
+            ]
+        },
+
+        endpoint () {
+            return `/questions/${this.id}/favorites`;
+        },
+
+        signedIn () {
+            return window.Auth.signedIn;
+        }
+    },
+
+    methods: {
+        toggle () {
+            if (!this.signedIn) {
+                this.$toast.warning('Please login to favorite this question', 'Warning', {
+                    timeout: 3000,
+                    position: 'bottomLeft'
+                })
+                return;
+            }
+            this.isFavorited ? this.destroy() : this.create();
+        },
+
+        destroy () {
+            axios.delete(this.endpoint)
+            .then(res => {
+                console.log('des-yes')
+                this.count--;
+                this.isFavorited = false;
+            })
+            .catch(err => {
+                console.log('des-no')
+                alert('fail');
+            })
+        },
+
+        create () {
+            axios.post(this.endpoint)
+            .then(res => {
+                console.log('cre-yes')
+                this.count++;
+                this.isFavorited = true;
+            })
+            .catch(err => {
+                console.log('cre-no')
+                alert('fail');
+            })
+        }
+    }
+
+}
+</script>
