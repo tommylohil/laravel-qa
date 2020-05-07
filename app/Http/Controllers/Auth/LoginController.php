d53318427b9a27141a1981f1ca052be6c716434d
tommylohil <tommylohil23052000@gmail.com>
2020-05-07 13:42:28 +0700

    Created get token api

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -36,5 +40,34 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    // Login
+    /**
+     * Login user
+     *
+     * @bodyParam email string required
+     * @bodyParam password string required
+     */
+    public function getToken(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response = [
+                    'token' => $token,
+                ];
+
+                return response($response, 200);
+            } else {
+                $response = 'Email atau Password salah, silahkan coba kembali';
+                return response()->json(['message' => $response], 422);
+            }
+        } else {
+            $response = 'Email atau Password salah, silahkan coba kembali';
+            return response()->json(['message' => $response], 422);
+        }
     }
 }
